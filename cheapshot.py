c870bb3d0a1fff11a0807c91e12155da9623f652
tomsaunders98 <tomandthenews@gmail.com>
2020-07-06 17:01:28 +0100

    test

--- cheapshot.py
+++ cheapshot.py
@@ -22,25 +22,30 @@ def checktoxic(tweets):
             date = tweets.loc[tweets["tweet"] == tweet, "date"].values[0]
             link = tweets.loc[tweets["tweet"] == tweet, "link"].values[0]
             result = predictions["results"][i]["predictions"]
-            if result["toxic"] > 0.5:
+            if result["toxic"] > 0.9:
                 tweetdata = [tweet, date, link, str(result["toxic"])]
                 if tweet not in toxictweets:
                     toxictweets.append(tweetdata)
-            if result["severe_toxic"] > 0.5:
+            if result["severe_toxic"] > 0.9:
                 tweetdata = [tweet, date, link, str(result["severe_toxic"])]
-                toxictweets.append(tweetdata)
-            if result["obscene"] > 0.5:
+                if tweet not in toxictweets:
+                    toxictweets.append(tweetdata)
+            if result["obscene"] > 0.9:
                 tweetdata = [tweet, date, link, str(result["obscene"])]
-                toxictweets.append(tweetdata)
-            if result["threat"] > 0.5:
+                if tweet not in toxictweets:
+                    toxictweets.append(tweetdata)
+            if result["threat"] > 0.9:
                 tweetdata = [tweet, date, link, str(result["threat"])]
-                toxictweets.append(tweetdata)
-            if result["insult"] > 0.5:
+                if tweet not in toxictweets:
+                    toxictweets.append(tweetdata)
+            if result["insult"] > 0.9:
                 tweetdata = [tweet, date, link, str(result["insult"])]
-                toxictweets.append(tweetdata)
-            if result["identity_hate"] > 0.5:
+                if tweet not in toxictweets:
+                    toxictweets.append(tweetdata)
+            if result["identity_hate"] > 0.9:
                 tweetdata = [tweet, date, link, str(result["identity_hate"])]
-                toxictweets.append(tweetdata)
+                if tweet not in toxictweets:
+                    toxictweets.append(tweetdata)
     if os.path.isfile("temp.csv"):
         os.remove("temp.csv")
     return toxictweets
@@ -54,11 +59,11 @@ def findtweets(usernames):
         c.Username = username
         c.Debug = True
         c.Store_csv = True
-        c.Custom_csv = ["tweet", "date"]
+        c.Custom_csv = ["tweet", "date", "link"]
         c.Output = "temp.csv"
         c.Hide_output = True
         twint.run.Search(c)
-        tweets = pd.read_csv("temp.csv")
+        tweets = pd.read_csv("temp.csv", low_memory = False)
         if len(tweets) > 0:
             print(f"Found {len(tweets)} tweets.")
             return checktoxic(tweets)
@@ -84,6 +89,7 @@ def get_parser():
     return parser
 
 def mainquery():
+    usernames = []
     parser = get_parser()
     opts = parser.parse_args()
     if not opts.username and not opts.file:
@@ -93,22 +99,30 @@ def mainquery():
         print("Cannot set both --file and --username")
         sys.exit(1)
     if opts.username:
-        usernames = [opts.username]
+        user = opts.username.replace("@",'')
+        usernames = [user]
     if opts.file:
         with open(opts.file) as f:
-            usernames = [line.rstrip() for line in f]
-    tweets = findtweets(usernames)
-    if len(tweets) == 0:
-        print("No bad tweets found.")
+            for line in f:
+                user = line.rstrip()
+                user = user.replace("@", "")
+                usernames.append(user)
+    if len(usernames) == 0:
+        print("No username entered.")
+        sys.exit(1)
     else:
-        print("Toxic Tweets found:")
-        print("-----------------------------------------------")
-        for tweet in tweets:
-
-            print(tweet[0])
-            print("---")
-            print(f"({tweet[1]}, {tweet[2]}, Confidence: {tweet[3]}%)")
+        tweets = findtweets(usernames)
+        if len(tweets) == 0:
+            print("No bad tweets found.")
+        else:
+            print("Toxic Tweets found:")
             print("-----------------------------------------------")
+            for tweet in tweets:
+
+                print(tweet[0])
+                print("---")
+                print(f"({tweet[1]}, {tweet[2]}, Confidence: {tweet[3]}%)")
+                print("-----------------------------------------------")
 
 
 
